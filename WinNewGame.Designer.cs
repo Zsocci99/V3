08c4171c94412d95f6e4779d223814169531c324
Zsocci99 <zsoltcsokasijr@gmail.com>
2022-05-30 20:08:10 +0200

    Basic Window Shape

--- WinNewGame.Designer.cs
+++ WinNewGame.Designer.cs
@@ -40,14 +40,12 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.butBack = new System.Windows.Forms.Button();
             this.butGameStart = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 0;
@@ -56,7 +54,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Location = new System.Drawing.Point(12, 38);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 1;
@@ -65,7 +63,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Location = new System.Drawing.Point(12, 64);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 13);
             this.label3.TabIndex = 2;
@@ -74,7 +72,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 116);
+            this.label4.Location = new System.Drawing.Point(12, 90);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 13);
             this.label4.TabIndex = 3;
@@ -83,7 +81,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 150);
+            this.label5.Location = new System.Drawing.Point(12, 116);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(56, 13);
             this.label5.TabIndex = 4;
@@ -91,42 +89,42 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(70, 9);
+            this.textBox1.Location = new System.Drawing.Point(84, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(70, 42);
+            this.textBox2.Location = new System.Drawing.Point(84, 38);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 6;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(70, 77);
+            this.textBox3.Location = new System.Drawing.Point(84, 64);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 7;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(70, 116);
+            this.textBox4.Location = new System.Drawing.Point(84, 90);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 8;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(70, 150);
+            this.textBox5.Location = new System.Drawing.Point(84, 116);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 9;
             // 
             // butBack
             // 
-            this.butBack.Location = new System.Drawing.Point(12, 176);
+            this.butBack.Location = new System.Drawing.Point(109, 142);
             this.butBack.Name = "butBack";
             this.butBack.Size = new System.Drawing.Size(75, 23);
             this.butBack.TabIndex = 10;
@@ -136,7 +134,7 @@
             // 
             // butGameStart
             // 
-            this.butGameStart.Location = new System.Drawing.Point(95, 176);
+            this.butGameStart.Location = new System.Drawing.Point(15, 142);
             this.butGameStart.Name = "butGameStart";
             this.butGameStart.Size = new System.Drawing.Size(88, 23);
             this.butGameStart.TabIndex = 11;
@@ -144,36 +142,11 @@
             this.butGameStart.UseVisualStyleBackColor = true;
             this.butGameStart.Click += new System.EventHandler(this.butGameStart_Click);
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 216);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(92, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Játékosok száma:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.comboBox1.Location = new System.Drawing.Point(49, 232);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 13;
-            // 
             // WinNewGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(207, 180);
             this.Controls.Add(this.butGameStart);
             this.Controls.Add(this.butBack);
             this.Controls.Add(this.textBox5);
@@ -208,7 +181,5 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button butBack;
         private System.Windows.Forms.Button butGameStart;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
